43bfb42cfa5b88a08ce51f5c57df83860f2198b6
lalaka32 <lalaka32@mail.ru>
2018-05-22 12:20:59 +0300

    OOP_8

---
+++ OOP/OOP_8/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string result { get; private set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            double a;
+            double b;
+            double c;
+            if (!double.TryParse(textBox1.Text,out a))
+            {
+                a = 0;
+            }
+            if (!double.TryParse(textBox2.Text, out b))
+            {
+                b = 0;
+            }
+            if (!double.TryParse(textBox3.Text, out c))
+            {
+                c = 0;
+            }
+
+            Сone cone = new Сone(c,b,a);
+
+            result += String.Format("Radius: {0}, Height: {1}, Density: {2} ",c,b,a);
+            if (checkBox2.CheckState == CheckState.Checked)
+            {
+                result += String.Format("Volume: {0} ", cone.Volume());
+            }
+            if (checkBox3.CheckState==CheckState.Checked)
+            {
+                result += String.Format("Mass: {0} ",cone.Mass());
+            }
+            
+            this.result = result;
+            Close();
+        }
+
+    }
+}
---
+++ OOP/OOP_8/WindowsFormsApp1/WindowsFormsApp1/Сone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Сone
+    {
+        double radius;
+        double height;
+        double density;
+
+        public Сone(double radus=0, double height=0, double density=0)
+        {
+            this.radius = radus;
+            this.height = height;
+            this.density = density;
+        }
+        public double Volume()
+        {
+            return (height*Math.PI*Math.Pow(radius,2)) / 3;
+        }
+        public double Mass()
+        {
+            return Volume() * density;
+        }
+    }
+}
